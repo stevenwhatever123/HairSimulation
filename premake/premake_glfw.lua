38a8c73a6124d1e95cd5dc5150d8b8a13a8022bd
stevenwhatever123 <60275831+stevenwhatever123@users.noreply.github.com>
2022-06-22 10:20:37 -0700

    Updated glfw premake

--- premake/premake_glfw.lua
+++ premake/premake_glfw.lua
@@ -7,48 +7,6 @@ project "glfw"
 
 	includedirs {"../libraries/glfw/include"}
 
-	vpaths {
-
-		['Header Files'] = {
-			"../libraries/glfw/include/GLFW/glfw3.h",
-			"../libraries/glfw/include/GLFW/glfw3native.h",
-			"../libraries/glfw/src/internal.h",
-			"../libraries/glfw/src/mappings.h",
-			"../libraries/glfw/src/null_joystick.h",
-			"../libraries/glfw/src/null_platform.h",
-			"../libraries/glfw/src/win32_platform.h",
-			"../libraries/glfw/src/win32_joystick.h",
-			"../libraries/glfw/src/win32_thread.h",
-			"../libraries/glfw/src/win32_time.h"
-		},
-
-		["Source Files"] = {
-
-			"../libraries/glfw/src/context.c",
-			"../libraries/glfw/src/egl_context.c",
-			"../libraries/glfw/src/init.c",
-			"../libraries/glfw/src/input.c",
-			"../libraries/glfw/src/monitor.c",
-			"../libraries/glfw/src/null_init.c",
-			"../libraries/glfw/src/null_joystick.c",
-			"../libraries/glfw/src/null_monitor.c",
-			"../libraries/glfw/src/null_window.c",
-			"../libraries/glfw/src/osmesa_context.c",
-			"../libraries/glfw/src/platform.c",
-			"../libraries/glfw/src/vulkan.c",
-			"../libraries/glfw/src/wgl_context.c",
-			"../libraries/glfw/src/window.c",
-
-			"../libraries/glfw/src/win32_init.c",
-			"../libraries/glfw/src/win32_joystick.c",
-			"../libraries/glfw/src/win32_module.c",
-			"../libraries/glfw/src/win32_monitor.c",
-			"../libraries/glfw/src/win32_thread.c",
-			"../libraries/glfw/src/win32_time.c",
-			"../libraries/glfw/src/win32_window.c",
-		}
-	}
-
 	files
 	{
 		"../libraries/glfw/include/GLFW/glfw3.h",
@@ -86,7 +44,8 @@ project "glfw"
 		"../libraries/glfw/src/win32_window.c",
 	}
 
-	defines {
+	defines 
+	{
 		"_GLFW_WIN32",
 		"_CRT_SECURE_NO_WARNINGS"
 	}
@@ -94,4 +53,8 @@ project "glfw"
 
 	filter "configurations:Debug"
 		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
 		symbols "on"
