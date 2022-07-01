d4ddadab73465c8e0bfb7a5b036d0cd55b1a5d9b
stevenwhatever123 <60275831+stevenwhatever123@users.noreply.github.com>
2022-07-01 05:58:45 -0700

    GLShader halfway done

--- headers/GLShader.h
+++ headers/GLShader.h
@@ -1,5 +1,13 @@
 #pragma once
+#include "Types.h"
+
 class GLShader
 {
+private:
+
+public:
+
+public:
+	GLuint loadShader(const char* vertexShaderPath, const char* fragmentShaderPath);
 };
 
--- src/GLShader.cpp
+++ src/GLShader.cpp
@@ -1,2 +1,67 @@
 #include "pch.h"
 #include "GLShader.h"
+
+GLuint GLShader::loadShader(const char* vertexShaderPath, const char* fragmentShaderPath)
+{
+	GLuint vertexShaderId = glCreateShader(GL_VERTEX_SHADER);
+	GLuint fragmentShaderId = glCreateShader(GL_FRAGMENT_SHADER);
+
+	// Read vertex shader ode from file
+	std::string vertexShaderCode;
+	std::ifstream vertexShaderStream(vertexShaderPath, std::ios::in);
+	if (vertexShaderStream.is_open())
+	{
+		std::stringstream sstr;
+		sstr << vertexShaderStream.rdbuf();
+		vertexShaderCode = sstr.str();
+		vertexShaderStream.close();
+	}
+	else
+	{
+		printf("Failed to read vertex shader: %s\n", vertexShaderPath);
+		return 0;
+	}
+
+	// Read fragment shader code from file
+	std::string fragmentShaderCode;
+	std::ifstream fragmentShaderStream(fragmentShaderPath, std::ios::in);
+	if (fragmentShaderStream.is_open())
+	{
+		std::stringstream sstr;
+		sstr << fragmentShaderStream.rdbuf();
+		fragmentShaderCode = sstr.str();
+		fragmentShaderStream.close();
+	}
+	else
+	{
+		printf("Failed to read fragment shader: %s\n", fragmentShaderPath);
+		return 0;
+	}
+
+	GLint ret = GL_FALSE;
+	int infoLogLength;
+
+	printf("Compiling shader: %s\n", vertexShaderPath);
+	char const* vertexSourcePointer = vertexShaderCode.c_str();
+	glShaderSource(vertexShaderId, 1, &vertexSourcePointer, NULL);
+	glCompileShader(vertexShaderId);
+
+	// Check Vertex Shader
+	glGetShaderiv(vertexShaderId, GL_COMPILE_STATUS, &ret);
+	glGetShaderiv(vertexShaderId, GL_INFO_LOG_LENGTH, &infoLogLength);
+
+	if (ret == GL_FALSE)
+	{
+		printf("Error compiling vertex shader: %s\n", vertexShaderPath);
+	}
+
+	if (infoLogLength > 0)
+	{
+		std::vector<char> VertexShaderErrorMessage(infoLogLength + 1);
+		glGetShaderInfoLog(vertexShaderId, infoLogLength, NULL, &VertexShaderErrorMessage[0]);
+		printf("%s\n", &VertexShaderErrorMessage[0]);
+	}
+
+	// Compile Fragment Shader
+
+}
